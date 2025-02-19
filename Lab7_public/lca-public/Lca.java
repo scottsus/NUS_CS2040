fc0b545b58436e427ba770c318dbfb4f2cebfcf9
scott susanto <susantoscott@gmail.com>
2025-02-18 18:18:38 -0800

    Lab 7 and Lab 8

--- Lab7_public/lca-public/Lca.java
+++ Lab7_public/lca-public/Lca.java
@@ -2,11 +2,64 @@ import java.util.*;
 
 public class Lca {
     private void run() {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(), Q = sc.nextInt();
 
+        TreeNode[] nodes = new TreeNode[N];
+        for (int i = 0; i < N; i++) {
+            nodes[i] = new TreeNode(sc.nextInt());
+        }
+        sc.nextLine();
+
+        for (int i = 0; i < N - 1; i++) {
+            String line = sc.nextLine();
+            String[] parts = line.split(" ");
+            String childDir = parts[0];
+            int parent = Integer.valueOf(parts[1]);
+            int child = Integer.valueOf(parts[2]);
+            if (childDir == "L") {
+                nodes[parent - 1].left = nodes[child - 1];
+            } else { // childDir == "R"
+                nodes[parent - 1].right = nodes[child - 1];
+            }
+        }
+
+        TreeNode root = nodes[0];
+        for (int i = 0; i < Q; i++) {
+            int child1 = sc.nextInt(), child2 = sc.nextInt();
+            int smaller = Math.min(child1, child2);
+            int larger = Math.max(child1, child2);
+            int lca = findLca(root, smaller, larger);
+            System.out.println(lca);
+        }
+
+        sc.close();
+    }
+
+    public int findLca(TreeNode root, int smaller, int larger) {
+        System.out.println(root.val);
+        if (smaller < root.val && larger < root.val) {
+            return findLca(root.left, smaller, larger);
+        }
+        if (smaller > root.val && larger > root.val) {
+            return findLca(root.right, smaller, larger);
+        }
+        return root.val;
     }
 
     public static void main(String args[]) {
         Lca runner = new Lca();
         runner.run();
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
     }
 }
--- Lab8_public/median-public/Median.java
+++ Lab8_public/median-public/Median.java
@@ -2,11 +2,60 @@ import java.util.*;
 
 public class Median {
     private void run() {
+        Scanner sc = new Scanner(System.in);
+        int Q = sc.nextInt();
 
+        MedianFinder mf = new MedianFinder();
+        for (int i = 0; i < Q; i++) {
+            int op = sc.nextInt();
+            if (op == 1) {
+                int num = sc.nextInt();
+                mf.offer(num);
+            } else { // op == 2
+                mf.printMedian();
+            }
+        }
+
+        sc.close();
     }
 
     public static void main(String args[]) {
         Median runner = new Median();
         runner.run();
+    }
+}
+
+class MedianFinder {
+    Queue<Integer> left, right;
+
+    MedianFinder() {
+        left = new PriorityQueue<>();
+        right = new PriorityQueue<>(Collections.reverseOrder());
+    }
+
+    void offer(int num) {
+        if (left.isEmpty() || left.peek() < num) {
+            left.offer(num);
+        } else {
+            right.offer(num);
+        }
+        if (left.size() > right.size() + 1) {
+            right.offer(left.poll());
+        }
+        if (left.size() < right.size()) {
+            left.offer(right.poll());
+        }
+    }
+
+    void printMedian() {
+        if (left.size() > right.size()) {
+            System.out.println(left.peek());
+            return;
+        }
+        if (left.isEmpty() || right.isEmpty()) {
+            System.out.println("None");
+            return;
+        }
+        System.out.println(right.peek() + " " + left.peek());
     }
 }
